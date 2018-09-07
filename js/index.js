770bd61376d7388cc164957388a73e115a872a51
Eileen Phan <evarina077@fullsail.edu>
2018-09-06 19:17:34 -0700

    Updted readme and changed to fetch

--- js/index.js
+++ js/index.js
@@ -1,66 +1,71 @@
-// Single state variable for endpoint
-var MD_BASE_URL = "https://api.themoviedb.org/3/search/movie?include_adult=false&page=1";
+const form = document.querySelector('.js-search-form');
 
-var state = {
-    query: ''
-};
+form.addEventListener('submit', (e) => {
 
-// State modification functions
-function getApiData(searchTerm, callback) {
-    var query = {
-        query: searchTerm,
-        language: 'en-US',
-        api_key: '7ab3cb18b4ad4a07bbd8bb01acfa7091',
-    };
-    $.getJSON(MD_BASE_URL, query, callback);
+const base = `https://api.themoviedb.org/3/search/movie?include_adult=false&page=1&`;
+
+const searchTerm = document.querySelector('#searchTerm').value;
+const language = `en-US&`;
+const api_key = `7ab3cb18b4ad4a07bbd8bb01acfa7091`;
+
+const url = `${base}query=${searchTerm}&language=${language}api_key=${api_key}`;
+
+const option = {
+    method: 'GET'
 }
 
-// Render functions
-function displayMDSearchData(data) {
+
+e.preventDefault()
+
+fetch(url)
+  .then( response => {
+    if (response.ok) {
+        return response.json()
+    } else {
+        throw response
+    }
+})
+  .then( data => {
     
-    var resultElement = '';
+    console.log(data)
+    
+let resultElement = '';
     
     if(data.results.length > 1) {
-        resultElement += '<section class="js-search-results clearfix">';
-        resultElement += '<h2>' + '<span>' + "Results for " + state.query + '</span>' + '</h2>';
+        resultElement += `<section class="js-search-results clearfix">
+                        <h2><span>Results for ${searchTerm}</span></h2>`;
         
         if(data.results)
         
         data.results.forEach(function(results){
-            resultElement += '<article>';
-            resultElement += '<div class="container">';
-            resultElement += '<img src="https://image.tmdb.org/t/p/w500' + results.poster_path + '"/>';
-            resultElement += '<div class="content">';
+            resultElement += `<article>
+                             <div class="container">
+                             <img src="https://image.tmdb.org/t/p/w500${results.poster_path}"/>
+                             <div class="content">`;
             if(results.title.length > 20) {
-               resultElement += '<h3>' + results.title.substr(0,20) +'...</h3>'; 
+               resultElement += `<h3>${results.title.substr(0,20)}...</h3>`; 
             } else {
-                resultElement += '<h3>' + results.title + '</h3>';             
+                resultElement += `<h3>${results.title}</h3>`;             
               }
              
-            resultElement += '<p>Released: ' + results.release_date + '</p>';
-            resultElement += '</div>';
-            resultElement += '</div>';
-            resultElement += '</article>';
+            resultElement += `<p>Released: ${results.release_date}</p>`;
+            resultElement += `</div>
+                              </div>
+                              </article>`;
         });
         
-        resultElement += '</section>';
+        resultElement += `</section>`;
         console.log(data);
         
     } else {
         resultElement += '<p class="no-results">No results</p>';
     }
     
-    $('.js-search-results').html(resultElement);
-}
-
-// Event listeners
-function watchSubmit() {
-  $('.js-search-form').submit(function(e) {
-    e.preventDefault();
-    state.query = $(this).find('.js-query').val();
-    var query = state.query;
-    getApiData(query, displayMDSearchData);
-    });
-  }
-
-$(function(){watchSubmit();});
+    document.querySelector('.js-search-results').innerHTML = resultElement;
+})
+    
+    .catch(err => {
+    console.log(err)
+    })
+    
+})
---
+++ readme.md
@@ -0,0 +1,28 @@
+# Project API Search App
+
+This project's purpose is to develop a basic site using scss, semantic html, javascript and API. I have built a basic search application using html, sass(scss), gulp, and javascript. It connects to an API that populate a page with the results. I chose to utilize the [Movie Database API](https://developers.themoviedb.org/3/getting-started/introduction).
+
+## Styled with HTML and SCSS Requirements
+
+* Variables for font family, font size, and an action color.
+* One mixin for a useful, reusable segment of styles.
+* Two uses of nesting, but never more than three levels deep.
+* One placeholder (%) and at least three uses of extend.
+* A folder structure consisting of at least 3 folders, 5 partial files, imported into a single main scss file.
+
+## Automated with Gulp
+
+A gulp file was created to handle compiling the scss files.
+
+## Generated resultes with the API
+
+* Captured search query from the search box and build the request url.
+* Made an AJAX request to the API endpoint.
+* Generated the results to match useful data from the API, conforming to the general layout provided.
+* Added a No Result output.
+
+## The MovieBox API Webpage
+
+[MovieBox](https://ephan077.github.io/project-setup-API-search-app/). If you'd like to run this locally please clone or download.
+
+![Screenshot of Todoist Desktop](images/clone.png)
