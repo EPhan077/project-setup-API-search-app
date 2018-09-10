ab74c91394b5f3d3f18c332d342554c08bd39183
Eileen Phan <evarina077@fullsail.edu>
2018-09-09 23:58:36 -0700

    corrected my gulpfile syntax

--- gulpfile.js
+++ gulpfile.js
@@ -2,24 +2,27 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var scss = './scss/*.scss';
 var browserSync = require('browser-sync').create();
+var postcss = require('gulp-postcss');
+var autoprefixer = require('autoprefixer');
 
 gulp.task('default', function() {
   // place code for your default task here
 })
 
 gulp.task('scss', function() {
+    
+  var processors = [
+      autoprefixer({browsers: ['last 2 versions']}),];
+    
   return gulp.src(scss)
     .pipe(sass())
+    .pipe(postcss(processors))
     .pipe(gulp.dest('./css'))
     .pipe(browserSync.reload({
       stream:true
   }))
 })
 
-
-gulp.task('watch', function() {
-    gulp.watch(scss, ['scss'])
-})
 
 gulp.task('browser-sync', function() {
           browserSync.init({
@@ -29,6 +32,6 @@ gulp.task('browser-sync', function() {
           })
 })
 
-gulp.task('watch', ['browser-sync', 'scss'], function() {
-    gulp.watch(scss, ['scss'])
-})
+gulp.task('watch', gulp.parallel('browser-sync', 'scss',function() {
+    gulp.watch(scss, gulp.parallel('scss'))
+}))
